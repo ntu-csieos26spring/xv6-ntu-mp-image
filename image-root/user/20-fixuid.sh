7b4f2c90e71340c8d72e65eca1fd768da897e22c
Typas Liao <r14944052@csie.ntu.edu.tw>
2026-04-08 09:19:43 +0800

    fix: tmp disable fixuid

--- image-root/user/20-fixuid.sh
+++ image-root/user/20-fixuid.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # fixuid: setuid root so it can remap student's UID to match -u flag
-chown root:root /usr/local/bin/fixuid
-chmod 4755 /usr/local/bin/fixuid
-mkdir -p /etc/fixuid
-printf "user: %s\ngroup: %s\npaths:\n  - %s\n" "${USER}" "${USER}" "${HOME}" > /etc/fixuid/config.yml
+#chown root:root /usr/local/bin/fixuid
+#chmod 4755 /usr/local/bin/fixuid
+#mkdir -p /etc/fixuid
+#printf "user: %s\ngroup: %s\npaths:\n  - %s\n" "${USER}" "${USER}" "${HOME}" > /etc/fixuid/config.yml
